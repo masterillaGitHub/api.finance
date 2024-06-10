f44682bcde526c188269aac9121a042196a2fab3
Masterilla <masterilla@ukr.net>
2024-06-10 15:05:14 +0300

    routes: web.php added information about cors

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
-
 require __DIR__.'/auth.php';
+
+Route::get('/', fn () => [
+    'Laravel' => app()->version(),
+    'session.domain' => config('session.domain'),
+    'cors.allowed_origins' => config('cors.allowed_origins'),
+    'app.url' => config('app.url'),
+    'app.frontend_url' => config('app.frontend_url'),
+    'sanctum.stateful' => config('sanctum.stateful'),
+]);
