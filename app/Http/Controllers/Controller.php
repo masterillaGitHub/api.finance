1d437daf1424d5bf14d1afcee547e1d08530ab94
Masterilla <masterilla@ukr.net>
2024-06-19 15:19:56 +0300

    Controller.php: Added method 'responseJsonData'

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function responseJsonData(array|int|string $data = []): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(['data' => $data]);
+    }
 }
