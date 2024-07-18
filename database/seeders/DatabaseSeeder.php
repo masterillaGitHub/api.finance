7afce5a14aca5422eb08d2f92688e7967b15b875
Masterilla <masterilla@ukr.net>
2024-07-18 16:27:59 +0300

    Migration: Added user seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             AccountSeeder::class,
             AccountSumSeeder::class,
             TransactionCategorySeeder::class,
