e106ef1024df0612ce82781509c72836cd862012
Masterilla <masterilla@ukr.net>
2024-10-28 18:38:49 +0200

    feat. Create repository and setting in controller for store 'BankConnection'

--- app/Http/Controllers/BankConnectionController.php
+++ app/Http/Controllers/BankConnectionController.php
@@ -2,27 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BankConnectionResource as Resource;
+use App\Repositories\BankConnectionRepository as Repository;
 use App\Http\Requests\BankConnection\StoreRequest as StoreRequest;
 use App\Services\BankConnectionService as Service;
-use Illuminate\Http\Response;
 
 class BankConnectionController extends Controller
 {
     private Service $service;
+    private Repository $repository;
 
     public function __construct()
     {
         $this->service = new Service();
+        $this->repository = new Repository();
     }
 
     /**
      * @throws \Throwable
      */
-    public function store(StoreRequest $request): Response
+    public function store(StoreRequest $request): Resource
     {
         $data = $request->validated();
-        $this->service->store($data);
 
-        return response()->noContent();
+        $item = $this->service->store($data);
+
+        $item = $this->repository->whereId($item->id);
+
+        return Resource::make($item);
     }
 }
---
+++ app/Repositories/BankConnectionRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BankConnection as Model;
+use Illuminate\Support\Collection;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class BankConnectionRepository extends CoreRepository
+{
+    protected function getModelClass(): string
+    {
+        return Model::class;
+    }
+
+    public function get(): Collection
+    {
+        return $this->query()->get();
+    }
+
+    public function whereId(int $id): ?Model
+    {
+        /** @var Model|null $res */
+        $res = $this->query()
+            ->where('id', $id)
+            ->first();
+
+        return $res;
+    }
+
+    public function query(): QueryBuilder
+    {
+        /** @var Model $model */
+        $model = $this->startConditions();
+
+        return $this->builder($model::query())
+            ->setAllowedIncludes([])
+            ->setAllowedFilters([])
+            ->build();
+
+    }
+}
